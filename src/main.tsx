0350f7aeb6cff1fffe87ca004f824419198dd276
Aurangzeb Sunny <royestheking@gmail.com>
2026-05-11 10:34:38 +0600

    Fix TypeScript IDE errors for PWA and Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import App from "./app/App.tsx";
+import App from "./app/App";
 import "./styles/index.css";
 import { registerSW } from 'virtual:pwa-register';
 
